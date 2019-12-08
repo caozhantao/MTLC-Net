fde502a34105b0e358c0c51423f88d54f753ccda
caozhantao <46522758@qq.com>
2019-12-08 21:26:48 +0800

    Add files via upload

---
+++ models/alexnet.py
@@ -0,0 +1,95 @@
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+import torch
+import copy
+
+__all__ = ['AlexNet', 'alexnet']
+
+
+model_urls = {
+    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
+}
+
+
+class AlexNet(nn.Module):
+
+    def __init__(self, num_classes=1000):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier_h = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096,num_classes),
+        )
+
+        self.classifier_s = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 5),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x_h = self.classifier_h(x)
+        x_s = self.classifier_s(x)
+
+        return x_h, x_s
+        #return x_2
+
+    
+
+def alexnet(pretrained=False, **kwargs):
+    r"""AlexNet model architecture from the
+    `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = AlexNet(**kwargs)
+    
+    if pretrained:
+
+        model.load_state_dict(torch.load('models/alexnet-owt-4df8aa71.pth'), strict=False)
+
+
+        mod = list(model.classifier_h.children())
+        mod.pop()
+        mod.append(torch.nn.Linear(4096, 2))
+        new_classifier = torch.nn.Sequential(*mod)
+        model.classifier_h = new_classifier
+
+        mod = list(model.classifier_s.children())
+        mod.pop()
+        mod.append(torch.nn.Linear(4096, 2))
+        new_classifier = torch.nn.Sequential(*mod)
+        model.classifier_s = new_classifier
+
+
+      
+
+    return model
